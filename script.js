f8a240b3dff7d383097647b913b6bcb17426ea67
Carlos Alexandre <karurosualexandresouza123@gmail.com>
2026-08-04 20:16:11 -0400

    Centralizando mapa

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const btnMenu = document.getElementById("btn-menu");
 const nav = document.querySelector(".navbar");
+const navLinks = document.querySelectorAll(".navbar a");
 
 btnMenu.addEventListener("click", () => {
-    nav.classList.toggle("active");
+  nav.classList.toggle("active");
 
-    if (nav.classList.contains("active")){
-        btnMenu.src = "https://img.icons8.com/ios/30/ffffff/cancel.png";
-    } else {
-        btnMenu.src = "https://img.icons8.com/ios/30/ffffff/menu--v1.png";
+  if (nav.classList.contains("active")) {
+    btnMenu.src = "https://img.icons8.com/ios/30/ffffff/cancel.png";
+  } else {
+    btnMenu.src = "https://img.icons8.com/ios/30/ffffff/menu--v1.png";
+  }
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (nav.classList.contains("active")) {
+      nav.classList.remove("active");
+      btnMenu.src = "https://img.icons8.com/ios/30/ffffff/menu--v1.png";
     }
-})
+  });
+});
